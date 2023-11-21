7a5d7757ff315774f51e80b82b86f0163fc3ecb3
tmKla <tm.klajn@gmail.com>
2023-11-21 16:40:46 +0100

    rework the project

--- web_app/server.js
+++ web_app/server.js
@@ -1,17 +1,90 @@
+/*
+nvm list available
+nvm install 18.17.1
+nvm use 18.17.1
+nvm version
+    1.1.11
+node -v
+    v18.17.1
+npm -v
+    9.6.7
+
+CD WEBAPP
+mkdir controllers models views
+npm init -y
+npm install mariadb express express-session body-parser passport dotenv ejs mysql2 cors
+node server.js
+*/
+
+/*
+const mariadb = require("mariadb");
+const pool = mariadb.createPool({
+    host: "localhost", user: "root", password: "", database: "carsdb"
+});
+pool.getConnection().then(function(conn){
+    conn.query("SELECT * FROM users").then(function(rows){ delete rows.meta; console.log(rows); });
+    conn.query("SELECT * FROM cars").then(function(rows){ delete rows.meta; console.log(rows); process.exit(); });
+});
+return;
+*/
+
+/*
+const mysql = require('mysql2/promise');
+const conn = mysql.createConnection({
+    host:'localhost', user: 'root', database: 'carsdb', password: '', debug: false
+});
+conn.then(function(conn) {
+    conn.execute('SELECT * FROM users').then(function(result){ const [rows, fields]=result; console.log(rows); });
+    conn.execute('SELECT * FROM cars').then(function(result){ const [rows, fields]=result; console.log(rows); process.exit(); });
+});
+return;
+*/
+
+const dotenv = require('dotenv');
+dotenv.config();
+
 const express = require('express');
 const app = express();
 app.set("view engine", "ejs");
 app.set("views", "views");
 
-// ca c'est une route simple, pour l'URL "/" il te répond response.send
-app.get('/', (request, response) => {
+app.listen(3003, '0.0.0.0',
+    function() { console.log("Listening on "+3003   ); }
+);
+
+app.get('/', (request, response) => { // 'GET' as a HTTP VERB, not as a 'getter'!
     let clientIp = request.ip;
     response.send(`Hello, dear ${clientIp}. I am a nodejs website...`);
     response.end(); // optional
 });
 
-// ca fait en sorte que tout ce qu'il y a dans le dossier /static soit accessible sur le site à /static (donc pour css, js, img)
-app.use("/static", express.static(__dirname + '/static'));
+// MIDDLEWARE REGISTRATIONS
+// app.use(callback1, callback2, callback3)
+const bodyParser = require("body-parser");
+app.use(bodyParser.json(), bodyParser.urlencoded({ extended: true }));
 
-// ça ça associe au chemin /hello toutes les routes qui sont décrites dans controllers/hello.route.js
+const session = require("express-session");
+app.use(session({
+    secret: "SecretRandomStringDskghadslkghdlkghdghaksdghdksh",
+    saveUninitialized: true,
+    cookie: { maxAge: 1000 * 60 * 60 * 24 }, // 1 day in msec
+    resave: false
+}));
+
+//if (request.session.cart===undefined) request.session.cart = [];
+//request.session.cart.push("xxxx");
+
+
+
+// app.use(routeBase, callback);
+app.use("/static", express.static(__dirname + '/static'));
 app.use("/hello", require("./controllers/hello.route"));
+
+// npm install cors
+var cors = require('cors');
+const { request } = require('express');
+app.use(cors());
+
+
+let value = "something";
+let other = `xxx yyy ${value} zzz`;
